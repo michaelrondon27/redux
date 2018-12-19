a5e63c122232af5b28bd92ec696735b7d97c619d
Michael Rondon <mrondon72@gmail.com>
2018-12-19 13:58:27 -0400

    16. Configurar mas acciones en nuestro reducer

--- 01-redux-basic/app-1.ts
+++ 01-redux-basic/app-1.ts
@@ -9,10 +9,23 @@ interface Action {
 // };
 
 function reducer( state = 10, action: Action ) {
-    
-    if ( action.type === 'INCREMENTAR' ) {
 
-        return state += 1;
+    switch ( action.type ) {
+
+        case 'INCREMENTAR':
+            return state += 1;
+
+        case 'DECREMENTAR':
+            return state -= 1;
+
+        case 'MULTIPLICAR':
+            return state * action.payload;
+
+        case 'DIVIDIR':
+            return state / action.payload;
+
+        default:
+            return state;
 
     }
 
@@ -27,3 +40,23 @@ const incrementadorAction: Action = {
 };
 
 console.log( reducer( 10, incrementadorAction ) );
+
+const decrementadorAction: Action = {
+    type: 'DECREMENTAR'
+};
+
+console.log( reducer( 10, decrementadorAction ) );
+
+const multiplicarAction: Action = {
+    type: 'MULTIPLICAR',
+    payload: 2
+};
+
+console.log( reducer( 10, multiplicarAction ) );
+
+const dividirAction: Action = {
+    type: 'DIVIDIR',
+    payload: 5
+};
+
+console.log( reducer( 10, dividirAction ) );
