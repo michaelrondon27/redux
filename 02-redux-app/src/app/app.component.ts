5a644709521ba13c635dd3f40b6101ff1a837272
Michael Rondon <mrondon72@gmail.com>
2018-12-22 22:07:57 -0400

    27. Action creator - Creador de acciones

--- 02-redux-app/src/app/app.component.ts
+++ 02-redux-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Store, Action } from '@ngrx/store';
+import { Store } from '@ngrx/store';
+import { IncrementarAction, DecrementarAction } from './contador/contador.actions';
 
 interface AppState {
   contador: number;
@@ -18,8 +19,6 @@ export class AppComponent {
     private store: Store<AppState>
   ) {
 
-    // this.contador = 10;
-
     this.store.subscribe( state => {
 
       this.contador = state.contador;
@@ -30,11 +29,7 @@ export class AppComponent {
 
   incrementar() {
 
-    // this.contador += 1;
-
-    const accion: Action = {
-      type: 'INCREMENTAR'
-    };
+    const accion = new IncrementarAction();
 
     this.store.dispatch( accion );
 
@@ -42,11 +37,7 @@ export class AppComponent {
 
   decrementar() {
 
-    // this.contador -= 1;
-
-    const accion: Action = {
-      type: 'DECREMENTAR'
-    };
+    const accion = new DecrementarAction();
 
     this.store.dispatch( accion );
 
