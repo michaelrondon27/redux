f94fae73ef5c2309f589f5a24974269cf7f5a595
Michael Rondon <mrondon72@gmail.com>
2019-02-03 01:58:06 -0400

    53. Cambiar el filtro -unicamente el string

--- 03-todoapp/src/app/todo/todo-footer/todo-footer.component.ts
+++ 03-todoapp/src/app/todo/todo-footer/todo-footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromFiltro from '../../filter/filter.actions';
+import { AppState } from '../../app.reducers';
 
 @Component({
   selector: 'app-todo-footer',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFooterComponent implements OnInit {
 
-  constructor() { }
+  filtrosValidos: fromFiltro.filtrosValidos[] = [
+    'todos',
+    'completados',
+    'pendientes'
+  ];
+
+  filtroActual: fromFiltro.filtrosValidos;
+
+  constructor(
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit() {
+
+    this.store.subscribe( state => {
+
+      this.filtroActual = state.filtro;
+
+    });
+
+  }
+
+  cambiarFiltro( nuevoFiltro: fromFiltro.filtrosValidos ) {
+
+    const accion = new fromFiltro.SetFiltroAction( nuevoFiltro );
+
+    this.store.dispatch( accion );
+
   }
 
 }
