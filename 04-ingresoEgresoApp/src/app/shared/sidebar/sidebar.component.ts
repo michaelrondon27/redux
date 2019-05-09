586020ed7adcad5bdd0d054465e8d9d182fb2c82
michael rondon <mrondon72@gmail.com>
2019-05-09 23:23:55 +0200

    108. Colocar el nombre del usuario activo

--- 04-ingresoEgresoApp/src/app/shared/sidebar/sidebar.component.ts
+++ 04-ingresoEgresoApp/src/app/shared/sidebar/sidebar.component.ts
@@ -1,18 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: []
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
+
+  nombre: string;
+
+  subscription: Subscription = new Subscription();
 
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    private store: Store<AppState>
   ) { }
 
   ngOnInit() {
+
+    this.subscription = this.store.select('auth').pipe(filter( auth => auth.user != null )).subscribe( auth => this.nombre = auth.user.nombre );
+
+  }
+
+  ngOnDestroy() {
+
+    this.subscription.unsubscribe();
+
   }
 
   logout() {
