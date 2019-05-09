86e229117927502c3592a7ab8ed0af85da86214b
michael rondon <mrondon72@gmail.com>
2019-05-09 20:22:40 +0200

    105. Dashboard - Estadistica basica

--- 04-ingresoEgresoApp/src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ 04-ingresoEgresoApp/src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { Subscription } from 'rxjs';
+import { IngresoEgreso } from '../ingreso-egreso.model';
 
 @Component({
   selector: 'app-estadistica',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticaComponent implements OnInit {
 
-  constructor() { }
+  cuantosErgesos: number;
+
+  cuantosIngresos: number;
+
+  egresos: number;
+
+  ingresos: number;
+
+  subscription: Subscription = new Subscription();
+
+  constructor(
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit() {
+
+    this.subscription = this.store.select('ingresoEgreso').subscribe( ingresoEgreso => {
+
+      this.contarIngresoEgreso( ingresoEgreso.items );
+
+    });
+
+  }
+
+  contarIngresoEgreso( items: IngresoEgreso[] ) {
+
+    this.cuantosErgesos = 0;
+
+    this.cuantosIngresos = 0;
+
+    this.egresos = 0;
+
+    this.ingresos = 0;
+
+    items.forEach( item => {
+
+      if ( item.tipo === 'ingreso' ) {
+
+        this.cuantosIngresos++;
+
+        this.ingresos += item.monto;
+
+      } else {
+
+        this.cuantosErgesos++;
+
+        this.egresos += item.monto;
+
+      }
+
+    });
+
   }
 
 }
