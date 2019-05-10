34b353fdd09e59fc6cb71b5f1f34a33994583513
mrondon <mrondon-externo@cnmc.es>
2019-05-10 10:43:37 +0200

    115. Modulo Share

--- 04-ingresoEgresoApp/src/app/app.module.ts
+++ 04-ingresoEgresoApp/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 // Modulos Personalizados
 import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 // Graficas
 import { ChartsModule } from 'ng2-charts';
@@ -28,9 +29,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
 import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
 import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { OrdenIngresoEgresoPipe } from './ingreso-egreso/orden-ingreso-egreso.pipe';
 
 @NgModule({
@@ -40,9 +38,6 @@ import { OrdenIngresoEgresoPipe } from './ingreso-egreso/orden-ingreso-egreso.pi
     IngresoEgresoComponent,
     EstadisticaComponent,
     DetalleComponent,
-    FooterComponent,
-    NavbarComponent,
-    SidebarComponent,
     OrdenIngresoEgresoPipe
   ],
   imports: [
@@ -53,6 +48,7 @@ import { OrdenIngresoEgresoPipe } from './ingreso-egreso/orden-ingreso-egreso.pi
     BrowserModule,
     ChartsModule,
     ReactiveFormsModule,
+    SharedModule,
     StoreModule.forRoot( appReducers ),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
---
+++ 04-ingresoEgresoApp/src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Components
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  exports: [
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
