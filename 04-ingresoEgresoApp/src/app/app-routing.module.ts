217e06737ded891fc80300a7b1b5e3392ec56adb
mrondon <mrondon-externo@cnmc.es>
2019-05-10 11:45:10 +0200

    119. CanLoad

--- 04-ingresoEgresoApp/src/app/app-routing.module.ts
+++ 04-ingresoEgresoApp/src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 
+// Guards
+import { AuthGuardService } from './auth/auth-guard.service';
+
 const routes: Routes = [
     {
         path: 'login',
@@ -16,7 +19,10 @@ const routes: Routes = [
     },
     {
         path: '',
-        loadChildren: './ingreso-egreso/ingreso-egreso.module#IngresoEgresoModule'
+        loadChildren: './ingreso-egreso/ingreso-egreso.module#IngresoEgresoModule',
+        canLoad: [
+            AuthGuardService
+        ]
     },
     {
         path: '**',
